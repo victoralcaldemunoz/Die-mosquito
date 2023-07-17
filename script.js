07451360218aa32ecd2c9c00363ceb86b52bae70
victor <vimusic_92@hotmail.com>
2023-07-17 20:00:31 +0100

    player created

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hola mundo')
+const board = document.getElementById('board');
+const player = new Mosquito(500, 250)
+
+function Mosquito (x, y){
+    this.x = x;
+    this.y = y;
+
+    this.sprite = document.createElement('div')
+
+    this.createMosquito = function(){
+        this.sprite.classList.add('mosquito')
+        this.sprite.style.left = this.x + 'px'
+        this.sprite.style.top = this.y + 'px'
+        board.appendChild(this.sprite)
+    }
+}
+
+player.createMosquito()
