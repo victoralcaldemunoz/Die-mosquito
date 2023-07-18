cf640b2379b79bc1e98deabb45a584698611cebe
Guacimara Ramirez Santana <guaci.rs10@gmail.com>
2023-07-18 17:40:18 +0100

    Comment to function

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-import { Player } from "./player.js";
+import { Player } from "./player.js";  // Importamos los datos de Player en el JS principal
+import { Enemy } from ".enemy.js"; // Importamos los datos del Enemigo en el JS principal
 
-const board = document.getElementById('board');
-let mosquito = new Player(0, 200, board);
-mosquito.createMosquito();
-let moveId;
 
-function movePlayer(){
-     moveId = setInterval(playerMove, 50)
+const board = document.getElementById('board'); //Traemos el container principal donde nos referenciaremos para crear las cosas desde el JS
+let mosquito = new Player(0, 200, board); // creamos un jugador con sus parametros.
+mosquito.createMosquito();  //Hacemos funcionar para que se cree nuestro jugador
+let moveId; //creamo una variable de movimiento
 
+function movePlayer(){ //Creamos una función para darle velocidad al jugador
+     moveId = setInterval(playerMove, 50) 
 }
 
-function playerMove(){
+function playerMove(){  //Creamos una función para crear el movimiento del jugador, dicho movimiento fue creada en el JS de Player.
     mosquito.move()
 }
 
-window.addEventListener('keydown', function(e){
+window.addEventListener('keydown', function(e){  //Creamos los botones de funcionamiento de nuestro jugador
     switch (e.key){
         case 'ArrowRight': 
             mosquito.directionX = 1;
@@ -31,7 +32,7 @@ window.addEventListener('keydown', function(e){
     }
 })
 
-window.addEventListener('keyup', function(e){
+window.addEventListener('keyup', function(e){  //
     if (e.key === 'ArrowRight' || e.key === 'ArrowLeft'){
         mosquito.directionX = 0;
         }
@@ -40,7 +41,7 @@ window.addEventListener('keyup', function(e){
     }
 })
 
-movePlayer();
+movePlayer(); //Hacemos funcionar al Jugador
 
 
 
